ae1df677dffb516e9f5b23ae9f2f0bbe9c3c59ed
faustin0 <dinatalefausto@gmail.com>
2021-02-16 07:55:23 +0000

    removing useless wrapping and flatten

--- src/user.ts
+++ src/user.ts
@@ -2,6 +2,8 @@ import { Email } from './email';
 import { isSome, Option } from 'fp-ts/lib/Option';
 import { option as O } from 'fp-ts';
 import { pipe } from 'fp-ts/lib/function';
+import { Either } from 'fp-ts/lib/Either';
+import * as E from 'fp-ts/lib/Either';
 
 export class User {
     private constructor(
@@ -10,20 +12,16 @@ export class User {
 
     static fromAddress(address: string): Option<User> {
         return pipe(
-            address,
-            O.fromNullable,
-            O.map(a => Email.fromAddress(a)),
-            O.filter(e => isSome(e)),
-            O.flatten,
+            Email.fromAddress(address),
             O.map(e => new User(e))
         )
     }
 
-
-    // static fromAddress(address: string): User {
-    //     const x = O.fold(
-    //         () => new Error(""),
-    //         e => new User(e))
-    //         (Email.fromAddress(address))
-    // }
+    static fromAddressE(address: string): Either<Error,User> {
+        return pipe(
+            Email.fromAddress(address),
+            O.map(e => new User(e)),
+            E.fromOption(()=> new Error("invalid email"))
+        )
+    }
 }
